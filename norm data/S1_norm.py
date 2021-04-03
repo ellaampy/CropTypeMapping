b316b533b6cb210c4594788240f5d0a32e8048cb
StellaO-A <ellaampy@gmail.com>
2021-04-03 20:34:09 +0200

    Update S1_norm.py

--- norm data/S1_norm.py
+++ norm data/S1_norm.py
@@ -1,3 +1,9 @@
+// stats generated for normalisation has been done using best effort to improve computational efficiency
+// for precise statistics, remove bestEffort parameter and set scale :10
+
+
+var department = ee.FeatureCollection('users/stella/cde/france_arrondissement')
+
 // department = department.filter(ee.Filter.eq('nom','Brest'))
 department = department.filter(ee.Filter.inList('nom', ee.List(['Brest', 'Morlaix', 'Quimper', 'Châteaulin'])))
 
@@ -18,3 +24,36 @@ var s1 = imageCollection2.filterDate('2018-10-01','2019-12-31').filterBounds(dep
 
 // Map.addLayer(s1.select('VV').mean(),{min: -25, max: 1}, 'Temporal average SAR image')
 print('initial collection', s1)
+
+// Combine the mean and standard deviation reducers.
+var reducers = ee.Reducer.mean().combine({
+  reducer2: ee.Reducer.stdDev(),
+  sharedInputs: true
+});
+
+
+var s1_setNorm = s1.map(function(img){
+  // return img.set('stats', img.reduceRegion({reducer:ee.Reducer.percentile([2, 98]), scale: 10, maxPixels:1e15}))})
+  return img.set({'stats': img.reduceRegion({reducer:ee.Reducer.percentile([2, 98]), bestEffort:true}), 'doa':ee.Date(img.get('system:time_start')).format('YYYYMMDD'),
+    'mean_std': img.reduceRegion({reducer:reducers, bestEffort:true})
+  })})
+
+
+
+print(s1_setNorm)
+
+var featList = s1_setNorm.map(function(img){
+  return ee.Feature(null).set({'doa': img.get('doa'), 'stats_percentile':img.get('stats'), 'mean_std':img.get('mean_std')})})
+
+
+Export.table.toDrive({
+  collection: featList,
+  folder: 'Earth Engine',
+  description:'S1_norm_data',
+  fileFormat: 'CSV',
+});
+
+
+---------------------------------------------------------------------------------------------------------------
+
+
